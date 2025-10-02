41033a9756913012956928930b73a5f553dc178a
Ger-B <germanignacio.bianco@estudiantes.unahur.edu.ar>
2025-10-02 17:54:10 -0300

    Agrega nav bar con reactbootstrap.

--- trabajociu/src/App.jsx
+++ trabajociu/src/App.jsx
@@ -2,6 +2,7 @@ import NavigationBar from "./components/NavigationBar";
 import Contacto from "./pages/Contacto";
 import Inicio from "./pages/Inicio";
 import About from "./pages/About";
+import Menu from "./pages/Menu";
 import {Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Inicio/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/contacto" element={<Contacto/>}/>
+        <Route path="/menu" element={<Menu/>}/>
         <Route path="*" element={<Navigate to="/"/>}/> {/* Podemos quizás hacer una pagina de error, u otra*/ }
       </Routes>
     </>
--- trabajociu/src/components/NavigationBar.jsx
+++ trabajociu/src/components/NavigationBar.jsx
@@ -1,30 +1,28 @@
-import {Link, NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown'; Por si lo queremos usar mas adelante
 
-export default function NavigationBar() {
-  return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <Link to="/" className="navbar-brand">Cafes</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <NavLink to="/" className="nav-link active" aria-current="page" >Home</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/contacto" className="nav-link">Contacto</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" >Pricing</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/about" className="nav-link">About</NavLink>
-                </li>
-            </ul>
-            </div>
-        </div>
-        </nav>
-  )
+function CollapsibleExample() {
+	return (
+		<Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" sticky="top">
+			<Container>
+				<Navbar.Brand href="#home">Luna & Granos - Café</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link to="/">Home</Nav.Link>
+						<Nav.Link to="/menu">Menú</Nav.Link>
+						<Nav.Link to="/contacto">Nosotros</Nav.Link>
+					</Nav>
+					<Nav>
+						<Nav.Link href="#deets">Carrito</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
+
+export default CollapsibleExample;
