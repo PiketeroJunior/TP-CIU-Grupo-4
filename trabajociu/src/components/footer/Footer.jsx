2dbf982afc1c88245b47647fd8031f99ff05ad26
PiketeroJunior <facundoromangimenez@gmail.com>
2025-10-02 03:37:26 -0300

    Agregado de footer y footer responsivo para tablets y celular

---
+++ trabajociu/src/components/footer/Footer.jsx
@@ -0,0 +1,44 @@
+import styles from "./Footer.module.css"
+
+
+export default function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <div className={styles["datos-footer"]}>
+                <div className={styles["item-footer"]}>
+                    <div className={styles["border-icon"]}>
+                        <a href="mailto:facundoroman.gimenez@estudiantes.unahur.edu.ar">
+                            <i className='bx bx-envelope'></i>
+                        </a>
+                    </div>
+                    <p className={styles["texto-footer"]}>Envía tu CV</p>
+                </div>
+                <div className={styles["item-footer"]}>
+                    <div className={styles["border-icon"]}>
+                        <a href="mailto:facundoromangimenez@gmail.com">
+                            <i className='bx bxl-gmail'></i>
+                        </a>
+                    </div>
+                    <p className={styles["texto-footer"]}>Consultas</p>
+                </div>
+                <div className={styles["item-footer"]}>
+                    <div className={styles["border-icon"]}>
+                        <a href="https://api.whatsapp.com/send?phone=541135932853">
+                            <i className='bx bxl-whatsapp'></i>
+                        </a>
+                    </div>
+                    <p className={styles["texto-footer"]}>Reserva</p>
+                </div>
+                <div className={styles["item-footer"]}>   
+                    <div className={styles["border-icon"]}>
+                        <a href="https://www.instagram.com/luna_coffee_brunch/?hl=es">
+                            <i className='bx bxl-instagram'></i>
+                        </a>
+                    </div>
+                    <p className={styles["texto-footer"]}>Síguenos</p>
+                </div> 
+            </div>
+            <h2 className={styles["titulo-final"]}>&copy; 2025 Grupo 4 CIU</h2>
+        </footer>
+    )
+}
--- trabajociu/src/pages/Inicio.jsx
+++ trabajociu/src/pages/Inicio.jsx
@@ -1,11 +1,13 @@
 import ImgPrincipal from "../components/ImgPrincipal/ImgPrincipal" 
 import Carrusel from "../components/Carrusel"
+import Footer from "../components/footer/Footer"
 
 export default function Inicio() {
   return (
     <>
         <ImgPrincipal/>
         <Carrusel/>
+        <Footer/>
     </>
   )
 }
